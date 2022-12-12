6bc84b8829fdb71047dc6b9e92da35c409eff626
JavierDelgadoFarro <delgadojavier238@gmail.com>
2022-12-12 13:20:31 -0500

    avance 29

---
+++ CapaDatos/Datos_Cliente.cs
@@ -0,0 +1,171 @@
+﻿using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class Datos_Cliente
+    {
+
+        public int Registrar(Entidad_Cliente obj, out string Mensaje)
+        {
+            int idAutogenerado = 0;
+            Mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+                    SqlCommand cmd = new SqlCommand("SP_RegistrarCliente", oconexion);
+                    cmd.Parameters.AddWithValue("@nombres", obj.Cli_nombres);
+                    cmd.Parameters.AddWithValue("@apellidos", obj.Cli_apellidos);
+                    cmd.Parameters.AddWithValue("@correo", obj.Cli_correo);
+                    cmd.Parameters.AddWithValue("@clave", obj.Cli_password);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 200).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    oconexion.Open();
+                    cmd.ExecuteNonQuery();
+                    idAutogenerado = Convert.ToInt32(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                idAutogenerado = 0;
+                Mensaje = ex.Message;
+            }
+            return idAutogenerado;
+        }
+
+
+        public List<Entidad_Cliente> Listar()
+        {
+            List<Entidad_Cliente> lista = new List<Entidad_Cliente>();
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+                    SqlCommand cmd = new SqlCommand("SP_ListarCliente", oconexion);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    oconexion.Open();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(
+                                new Entidad_Cliente()
+                                {
+                                    idCliente = Convert.ToInt32(dr["idCliente"]),
+                                    Cli_nombres = dr["Cli_nombres"].ToString(),
+                                    Cli_apellidos = dr["Cli_apellidos"].ToString(),
+                                    Cli_correo  = dr["Cli_correo"].ToString(),
+                                    Cli_password = dr["Cli_password"].ToString(),
+                                    Cli_reestablecer = Convert.ToBoolean(dr["Cli_reestablecer"]),
+                                }
+                            );
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                lista = new List<Entidad_Cliente>();
+            }
+            return lista;
+        }
+
+        
+
+
+        public bool Editar(Entidad_Cliente obj, out string Mensaje)
+        {
+            bool resultado = false;
+            Mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+                    SqlCommand cmd = new SqlCommand("SP_EditarCLiente", oconexion);
+                    cmd.Parameters.AddWithValue("idCliente", obj.idCliente);
+                    cmd.Parameters.AddWithValue("@nombres", obj.Cli_nombres);
+                    cmd.Parameters.AddWithValue("@apellidos", obj.Cli_apellidos);
+                    cmd.Parameters.AddWithValue("@correo", obj.Cli_correo);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 200).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    oconexion.Open();
+                    cmd.ExecuteNonQuery();
+                    resultado = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                resultado = false;
+                Mensaje = ex.Message;
+            }
+            return resultado;
+        }
+
+
+        public bool Eliminar(int id, out string Mensaje)
+        {
+            bool resultado = false;
+            Mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+                    SqlCommand cmd = new SqlCommand("SP_EliminarCliente", oconexion);
+                    cmd.Parameters.AddWithValue("IdCliente", id);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    oconexion.Open();
+                    cmd.ExecuteNonQuery();
+                    resultado = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                resultado = false;
+                Mensaje = ex.Message;
+            }
+            return resultado;
+        }
+
+        public bool CambiarClave(int idCliente, string nuevaclave, out string Mensaje)
+        {
+            bool resultado = false;
+            Mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+                    SqlCommand cmd = new SqlCommand("SP_CambiarClave_Cliente", oconexion);
+                    cmd.Parameters.AddWithValue("@id", idCliente);
+                    cmd.Parameters.AddWithValue("@nuevaclave", nuevaclave);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    oconexion.Open();
+                    cmd.ExecuteNonQuery();
+                    resultado = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                resultado = false;
+                Mensaje = ex.Message;
+            }
+            return resultado;
+        }
+    }
+}
--- CapaPresentacionTienda/App_Start/BundleConfig.cs
+++ CapaPresentacionTienda/App_Start/BundleConfig.cs
@@ -11,18 +11,35 @@ namespace CapaPresentacionTienda
             bundles.Add(new Bundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        //"~/Scripts/jquery.validate*"));
+            bundles.Add(new Bundle("~/bundles/complementos").Include(
+                        "~/Scripts/fontawesome/all.min.js",
+                        "~/Scripts/DataTables/jquery.dataTables.js",
+                        "~/Scripts/DataTables/dataTables.responsive.js",
+                        "~/Scripts/loadingoverlay/loadingoverlay.min.js",
+                        "~/Scripts/sweetalert.min.js",
+                        "~/Scripts/jquery.validate.js",
+                        "~/Scripts/jquery-ui.js",
+                        "~/Scripts/scripts.js"));
+
+            // bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //"~/Scripts/jquery.validate*"));
 
             // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información sobre los formularios.  De esta manera estará
             // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
             //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        //"~/Scripts/modernizr-*"));
+            //"~/Scripts/modernizr-*"));
 
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.bundle.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/site.css",
+                "~/Content/DataTables/css/jquery.dataTables.css",
+                "~/Content/DataTables/css/responsive.dataTables.css",
+                "~/Content/sweetalert.css",
+                "~/Content/jquery-ui.css"
+            ));
+
         }
     }
 }
